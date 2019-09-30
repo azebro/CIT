97a7e889b80a36c03b9a5a9955dec2f281db731e
Adam Zebrowski <azebro@hotmail.com>
2019-09-30 11:01:36 +0300

    .

---
+++ COMP9061/Lab2/lab2.py
@@ -0,0 +1,89 @@
+
+
+import os
+from math import log
+from collections import Counter
+
+
+
+def countOccurances(folder):
+    allWords = []
+    files = os.listdir(folder)
+    for file in files:
+        f = open(folder + file, 'r', encoding='utf8')
+        allWords += f.read().lower().split()
+        f.close()
+    return Counter(allWords), len(allWords), len(files)
+
+
+def countPw(vocabulary, totalNumberOfWords):
+    V = sum(vocabulary.values())
+    pw  = {}
+    for item in vocabulary:
+        pw[item] =  (vocabulary[item] + 1) / (totalNumberOfWords + V)
+    return pw
+
+def trainDataset(pathToPositive, pathToNegative):
+    countsNegative, allNegative, instancesNegative = countOccurances(pathNegative)
+    countsPositive , allPositive, instancesPositive = countOccurances(pathPositive)
+    pWpos = countPw(countsPositive, allPositive)
+    pWneg = countPw(countsNegative, allNegative)
+    cPos = instancesPositive / (instancesPositive + instancesNegative)
+    cNeg = instancesNegative / (instancesNegative + instancesPositive)
+    return pWpos, pWneg, cPos, cNeg
+
+
+def calculatePwC(data, pWs):
+    probabilitySum = 0
+    for k, v in data.items():
+        if k in pWs:
+            probabilitySum += log((v * pWs[k]))
+    return probabilitySum
+
+def classify(data, cP, cN, pWpos, pWneg):
+    pPosData = log(cP) + calculatePwC(data, pWpos)
+    pNegData = log(cN) + calculatePwC(data, pWneg)
+    if pPosData > pNegData:
+        return True
+    return False
+
+pathNegative = 'C:\\Users\\adamze\\Downloads\\data\\train\\neg\\'
+pathPositive = 'C:\\Users\\adamze\\Downloads\\data\\train\\pos\\'
+
+pWpos, pWneg, cP, cN = trainDataset(pathPositive, pathNegative)
+
+def test(folder, c):
+    files = os.listdir(folder)
+    outcomes = []
+    for file in files:
+        f = open(folder + file, 'r', encoding='utf8')
+        allWords = f.read().lower().split()
+        f.close()
+        outcome = classify(Counter(allWords), cP, cN, pWpos, pWneg)
+        outcomes.append(outcome)
+        #print("Classification for file:" + file + " : " + str(outcome ))
+    accuracy = len([o for o in outcomes if o == c]) / len(files)
+    print("Accuracy for: " + str(c) + " is " + str(accuracy))
+    return
+
+test('C:\\Users\\adamze\\Downloads\\data\\test\\neg\\', False)
+test('C:\\Users\\adamze\\Downloads\\data\\test\\pos\\', True)
+
+
+#print(pWpos)
+#print(pWneg)
+
+#uniqueWords = set(allWords)
+
+
+#print(counts)
+#print(countsNegative)
+#print(countsPositive)
+#print(pWpos)
+#print(pWneg)
+
+
+
+
+
+
